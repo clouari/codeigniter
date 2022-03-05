fd851bfa03932c3e2b2c470d60389813c6a7effc
Da hee <ekdudekgml35@naver.com>
2022-03-05 15:26:09 +0900

    Codeignoter: 글 작성하는 Form>Input

---
+++ html/application/views/board/input.php
@@ -0,0 +1,8 @@
+<center>
+<form method="post" action="/index.php/form/board_insert">
+<input type="text" name="title" placeholder="제목"> <br />
+<input type="text" name="name" placeholder="작성자"> <br />
+<textarea name="content" placeholder="내용"></textarea><br />
+    <input type="submit" value="글등록">
+</form>
+</center>
